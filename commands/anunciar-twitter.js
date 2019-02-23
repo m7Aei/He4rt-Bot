a1d0393f407bb15c644f356dc3203a26a1e58ff3
Gustavo M <gugumllhlimaa@gmail.com>
2019-02-23 15:28:05 -0300

    Update twitter command

--- commands/anunciar-twitter.js
+++ commands/anunciar-twitter.js
@@ -11,11 +11,14 @@ var T = new Twit({
     access_token:         process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
     timeout_ms:           60*1000,
-    strictSSL:            true,     
+    strictSSL:            true,
 })
 */
 
 const host = "http://danielgreis.me:8000/users?"
+
+module.exports = {
+    run: (client, message, args) => {
 
 var Day = new Date().toISOString().split("T")[0];
 var id_users = []
@@ -24,29 +27,18 @@ request(`${host}${Day}`, (err, res, body) => {
     info['data'].forEach(element => {
         id_users.push(element['discord_id'])
     });
-    console.log(id_users)
-
-
-    module.exports = {
-        run: (client, message, args) => {
-            var name = client.user.get('id', id_users[1]);
-            console.log(name)
+            console.log(id_users)
+            var name = client.guilds.get(process.env.GUILD_ID).members.get(id_users[1]);
+            console.log(`${name.displayName}`)
+            })
         },
-    
+
         get command() {
             return {
               name: 'twitter',
               category: 'Moderator',
-              description: 'Divulgação de novos usuarios no tt',
-              usage: 'Twitter'
+              description: 'Divulgação de novos usuarios no twitter.',
+              usage: 'twitter'
             }
         }
     }
-
-})
-
-
-
-
-
-
